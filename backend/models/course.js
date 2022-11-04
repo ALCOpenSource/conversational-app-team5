bd229a1e26b1003479cf3a42905f8d2f7c4eac2d
ayo <2017630+aaademosu@users.noreply.github.com>
2022-11-04 23:35:50 +0100

    created course model

---
+++ backend/models/course.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+/**
+ * We can create the content field as a buffer. That way we can do,
+
+  const a = new Course({ content: {type: 'Buffer', data: 'link to youtube video'}}); // {"type":"Buffer","data": "url" }
+
+  const b = new Course({ content: bufferStream }); // {"type":"Buffer","data":[serialized buffer content in an array]}
+
+*/
+
+
+const schema = new mongoose.Schema({
+  author_id: {
+    required: true,
+    type: String
+  },
+  title: {
+    required: true,
+    type: String
+  },
+  tags: {
+    required: true,
+    type: Array
+  },
+  description: {
+    required: true,
+    type: String
+  },
+  contentType: {
+    required: true,
+    type: String,
+    enum: ['video', 'link', 'text']
+  },
+  content: {
+    required: true,
+    type: String
+  },
+  timestamp: {
+    required: true,
+    type: Date
+  },
+});
+
+const Course = mongoose.model('courses', schema);
+
+module.exports = Course;
