635786257c6493b5fba2409fb980f7d3962b60a6
ayo <2017630+aaademosu@users.noreply.github.com>
2022-11-18 11:49:35 +0100

    changes related to creating courses

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,34 +1,39 @@
 import axios from "axios";
+import { getToken } from '../contexts/auth';
 
-
-const token =  localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
 export const GetCourses = async () => {
-    try {
-        const data = await axios.get(`https://masterminds-api.herokuapp.com/v1/courses`, 
-            {
-              headers: { 
-                  "Authorization": token
-              },
-            })
-          console.log("Get Courses",data);
-           return data; 
-          } catch (error) {
-              console.log(error)  
-          }
+  try {
+    const data = await axios.get(
+      `https://masterminds-api.herokuapp.com/v1/courses`, {
+      headers: {
+          "Authorization": token
+      },
+    });
+    console.log("Get Courses",data);
+    return data;
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  export const PostCourses = async (body) => {
-    try {
-        const data = await axios.post(`https://masterminds-api.herokuapp.com/v1/courses`, body, 
-            {
-              headers: { 
-                  "Authorization": token
-              },
-            })
-          console.log("Post Courses",data);
-           return data; 
-          } catch (error) {
-              console.log(error)  
-          }
+export const PostCourses = async (body) => {
+  const token = await getToken();
+
+  console.log(token)
+
+  try {
+    const data = await axios.post(
+      `https://masterminds-api.herokuapp.com/v1/courses`, body, {
+      headers: {
+        "Authorization": token
+      },
+    });
+    console.log("Post Courses",data)
+    return data;
+  } catch (error) {
+    console.log(error)
+    return error;
   }
+}
