ae1dde05677e38376fd55139ef1c7263ea91355f
ayo <2017630+aaademosu@users.noreply.github.com>
2022-11-25 16:47:43 +0100

    added socket io and persistent session and chat implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,49 @@
 const cors = require('cors');
+const http = require('http');
 const express = require('express');
-const config = require("../config");
+const ioSocket = require("socket.io");
 const bodyParser = require('body-parser');
 const createError = require('http-errors');
 
+const config = require("../config");
+const { setupSocket } = require("./socket");
 const app = express();
-app.use(cors());
+
+app.use(cors()); // // app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Setup Database
-require('../database')();
-
 const UsersRoute = require('../routes/users');
 const CoursesRoute = require('../routes/courses');
 const authMiddleWare = require('../auth/index');
+
 
 /**
  *
  * app.use(authMiddleWare);
 */
+const server = http.createServer(app);
 
+const io = ioSocket(server, {
+  cors: {
+    origin: config.appUrl,
+  },
+});
 
+setupSocket(io);
 
 app.use('/v1/users', authMiddleWare, UsersRoute);
 app.use('/v1/courses', authMiddleWare, CoursesRoute);
 app.use('/v1/status', (req, res, next) => {
   return res.json({ message: 'Hello World.' });
 });
+
+/*
 //404 handler and pass to error handler
+*/
 app.use((req, res, next) => {
   next(createError(404, 'Not found'));
 });
@@ -46,7 +58,9 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Setup Database
+require('../database')();
 
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   console.log('Server started on port ' + config.port + '...');
 });
